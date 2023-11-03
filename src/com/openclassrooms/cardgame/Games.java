d851f1885d31132bb453377dfbd4443048e844ea
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-11-03 10:01:15 +0100

    refacto & Open/Closed principe

---
+++ src/com/openclassrooms/cardgame/Games.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.cardgame;
+
+import com.openclassrooms.cardgame.controller.GameController;
+import com.openclassrooms.cardgame.games.GameEvaluator;
+import com.openclassrooms.cardgame.model.Deck;
+import com.openclassrooms.cardgame.view.View;
+
+public class Games {
+    public static void main(String[] args) {
+        GameController gc = new GameController(new Deck(), new View(), new GameEvaluator());
+        gc.run();
+    }
+}
--- src/com/openclassrooms/cardgame/model/Hand.java
+++ src/com/openclassrooms/cardgame/model/Hand.java
@@ -1,4 +1,4 @@
-package com.openclassroom.cardgame.model;
+package com.openclassrooms.cardgame.model;
 
 import java.util.ArrayList;
 import java.util.List;
