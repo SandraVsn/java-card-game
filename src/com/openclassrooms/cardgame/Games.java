7d8ff823e018e3c6fc9fd19d422c46d8b0739a77
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-11-03 17:54:27 +0100

    Observator Pattern

--- src/com/openclassrooms/cardgame/Games.java
+++ src/com/openclassrooms/cardgame/Games.java
@@ -7,15 +7,34 @@ import com.openclassrooms.cardgame.model.Deck;
 import com.openclassrooms.cardgame.model.DeckFactory;
 import com.openclassrooms.cardgame.model.DeckFactory.DeckType;
 import com.openclassrooms.cardgame.view.ComandLineView;
+import com.openclassrooms.cardgame.view.GameSwingPassiveView;
 import com.openclassrooms.cardgame.view.GameSwingView;
+import com.openclassrooms.cardgame.view.GameViewables;
 
 public class Games {
     public static void main(String[] args) {
-        // GameController gc = new GameController(new Deck(), new ComandLineView(), new
-        // HightCardGameEvaluator());
-        GameSwingView view = new GameSwingView();
-        view.createAndShowGUI();
-        GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), view,
+
+        GameViewables views = new GameViewables();
+
+        GameSwingView gsv = new GameSwingView();
+        views.addViewable(gsv);
+
+        for (int i = 0; i < 3; i++) {
+            GameSwingPassiveView passiveView = new GameSwingPassiveView();
+            passiveView.createAndShowGUI();
+
+            views.addViewable(passiveView);
+
+            // sleep to move new Swing frame on window
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        gsv.createAndShowGUI();
+        GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), views,
                 new HightCardGameEvaluator());
 
         gc.run();
