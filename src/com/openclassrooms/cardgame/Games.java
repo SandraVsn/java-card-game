662c54864c53f7660e76b724812cf741cb14d198
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-11-03 10:12:36 +0100

    Open / Closed : add HightCardRule and LowCard rule with rule interface

--- src/com/openclassrooms/cardgame/Games.java
+++ src/com/openclassrooms/cardgame/Games.java
@@ -1,13 +1,17 @@
 package com.openclassrooms.cardgame;
 
 import com.openclassrooms.cardgame.controller.GameController;
-import com.openclassrooms.cardgame.games.GameEvaluator;
+import com.openclassrooms.cardgame.games.HightCardGameEvaluator;
+import com.openclassrooms.cardgame.games.LowCardGameEvaluator;
 import com.openclassrooms.cardgame.model.Deck;
 import com.openclassrooms.cardgame.view.View;
 
 public class Games {
     public static void main(String[] args) {
-        GameController gc = new GameController(new Deck(), new View(), new GameEvaluator());
+        // GameController gc = new GameController(new Deck(), new View(), new
+        // HightCardGameEvaluator());
+        GameController gc = new GameController(new Deck(), new View(), new LowCardGameEvaluator());
+
         gc.run();
     }
 }
