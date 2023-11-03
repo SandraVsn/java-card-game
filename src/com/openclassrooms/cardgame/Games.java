c8707a19d17d777c2640f72ab23cd3c11ac99968
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-11-03 11:21:41 +0100

    Dependency inversion

--- src/com/openclassrooms/cardgame/Games.java
+++ src/com/openclassrooms/cardgame/Games.java
@@ -5,12 +5,15 @@ import com.openclassrooms.cardgame.games.HightCardGameEvaluator;
 import com.openclassrooms.cardgame.games.LowCardGameEvaluator;
 import com.openclassrooms.cardgame.model.Deck;
 import com.openclassrooms.cardgame.view.ComandLineView;
+import com.openclassrooms.cardgame.view.GameSwingView;
 
 public class Games {
     public static void main(String[] args) {
         // GameController gc = new GameController(new Deck(), new ComandLineView(), new
         // HightCardGameEvaluator());
-        GameController gc = new GameController(new Deck(), new ComandLineView(), new LowCardGameEvaluator());
+        GameSwingView view = new GameSwingView();
+        view.createAndShowGUI();
+        GameController gc = new GameController(new Deck(), view, new HightCardGameEvaluator());
 
         gc.run();
     }
